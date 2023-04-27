5a5ad31533646be4b73e4b794bee35761fa781f3
Данила Вальковец <dahnh33@gmail.com>
2023-04-27 20:39:47 +0300

    Add let's encrypt certification and enable SSL

--- src/main/java/ru/ntdv/proicis/Application.java
+++ src/main/java/ru/ntdv/proicis/Application.java
@@ -1,11 +1,16 @@
 package ru.ntdv.proicis;
 
+import com.github.valb3r.letsencrypthelper.tomcat.TomcatWellKnownLetsEncryptChallengeEndpointConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
+@Import(TomcatWellKnownLetsEncryptChallengeEndpointConfig.class)
 @SpringBootApplication
-public class Application {
-    public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
-    }
+public
+class Application {
+public static
+void main(String[] args) {
+    SpringApplication.run(Application.class, args);
+}
 }
