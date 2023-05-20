50f69f1a8a751fe7884defc58f19ad09def45311
Данила Вальковец <dahnh33@gmail.com>
2023-05-20 21:56:04 +0300

    Fix TransactionRequiredException:

--- src/main/java/ru/ntdv/proicis/crud/service/SecretCodeService.java
+++ src/main/java/ru/ntdv/proicis/crud/service/SecretCodeService.java
@@ -4,6 +4,7 @@ import jakarta.annotation.Nullable;
 import lombok.val;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import ru.ntdv.proicis.buisness.robot.RobotStateManager;
 import ru.ntdv.proicis.crud.model.SecretCode;
 import ru.ntdv.proicis.crud.model.User;
@@ -31,6 +32,7 @@ boolean hasUnexpired() {
     return secretCodeRepository.existsByExpiresAfter(OffsetDateTime.now());
 }
 
+@Transactional
 public
 void deleteAllExpired() {
     secretCodeRepository.deleteAllByExpiresAfter(OffsetDateTime.now());
