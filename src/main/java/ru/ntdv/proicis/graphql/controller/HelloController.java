7e79e074a19d4c9a7625ad339011f9022e3190c8
alex <nosoval3ksey@yandex.ru>
2023-07-05 01:19:18 +0300

    Watching how request work 6.

--- src/main/java/ru/ntdv/proicis/graphql/controller/HelloController.java
+++ src/main/java/ru/ntdv/proicis/graphql/controller/HelloController.java
@@ -17,11 +17,11 @@ public class HelloController {
     @Autowired
     private HttpServletRequest request;
     @QueryMapping
-    public HttpServletRequest getCurrentUserDateTime(){
-        return request;
-    }
-//    public TimeZone getCurrentUserDateTime(){
-//        return RequestContextUtils.getTimeZone(request);
-//        return now((RequestContextUtils.getTimeZone(request)).toZoneId());
+//    public HttpServletRequest getCurrentUserDateTime(){
+//        return request;
 //    }
+    public TimeZone getCurrentUserDateTime(){
+        return RequestContextUtils.getTimeZone(request);
+//        return now((RequestContextUtils.getTimeZone(request)).toZoneId());
+    }
 }
