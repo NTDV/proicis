f0310e52de371f0d6deeab9cf503107c6dcaf0c0
alex <nosoval3ksey@yandex.ru>
2023-07-05 00:12:19 +0300

    Watching how request work 3.

--- src/main/java/ru/ntdv/proicis/graphql/controller/HelloController.java
+++ src/main/java/ru/ntdv/proicis/graphql/controller/HelloController.java
@@ -1,5 +1,6 @@
 package ru.ntdv.proicis.graphql.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.servlet.support.RequestContextUtils;
@@ -11,8 +12,11 @@ import static java.time.OffsetDateTime.now;
 
 @Controller
 public class HelloController {
+    @Autowired
+    private HttpServletRequest request;
     @QueryMapping
-    public OffsetDateTime getCurrentUserDateTime(HttpServletRequest request){
+    public OffsetDateTime getCurrentUserDateTime(){
+
         return now((RequestContextUtils.getTimeZone(request)).toZoneId());
     }
 }
