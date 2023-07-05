39dfe5b4a21048b18a43aa20ca258aacc1ac76bd
Данила Вальковец <dahnh33@gmail.com>
2023-07-05 14:57:15 +0300

    Может, я что-то напутал и это так не работает?

--- src/main/java/ru/ntdv/proicis/graphql/controller/HelloController.java
+++ src/main/java/ru/ntdv/proicis/graphql/controller/HelloController.java
@@ -1,21 +1,25 @@
 package ru.ntdv.proicis.graphql.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.servlet.support.RequestContextUtils;
 
 import java.time.OffsetDateTime;
-import java.util.TimeZone;
+import java.util.Objects;
 
 import static java.time.OffsetDateTime.now;
 
 @Controller
 public
 class HelloController {
-//    @Autowired
-//    private HttpServletRequest request;
+@Autowired
+private HttpServletRequest request;
+
 @QueryMapping
-public static
-OffsetDateTime getCurrentUserDateTime(TimeZone timeZone) {
-    return now(timeZone.toZoneId());
+public
+OffsetDateTime getCurrentUserDateTime() {
+    return now(Objects.requireNonNull(RequestContextUtils.getTimeZone(request)).toZoneId());
 }
 }
