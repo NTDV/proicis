8b29a53bbcc1cf2289eeb0d5ddc2cb008e69937d
alex <nosoval3ksey@yandex.ru>
2023-07-09 00:50:53 +0300

    Make data look ok

--- src/main/java/ru/ntdv/proicis/graphql/controller/HelloController.java
+++ src/main/java/ru/ntdv/proicis/graphql/controller/HelloController.java
@@ -2,11 +2,14 @@ package ru.ntdv.proicis.graphql.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.OffsetDateTime;
 import java.util.Date;
 import java.util.TimeZone;
 
 
 import static java.time.OffsetDateTime.now;
+import static java.time.OffsetDateTime.of;
 
 @RestController
 public
@@ -14,6 +17,8 @@ class HelloController {
     @RequestMapping(path = "/getTime")
     public
     String getCurrentUserDateTime(TimeZone timeZone) {
-        return Date.from(now(timeZone.toZoneId()).toInstant()).toString();
+        OffsetDateTime data = now(timeZone.toZoneId());
+        return "<h1>Is this work?</h1>";
+//        return Date.from(now(timeZone.toZoneId()).toInstant()).toString();
     }
 }
