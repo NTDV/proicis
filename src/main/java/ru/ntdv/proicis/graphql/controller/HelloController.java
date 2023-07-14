e84f1f9cd11560f6b58f2608a8807e2121c02ffd
alex <nosoval3ksey@yandex.ru>
2023-07-14 15:20:01 +0300

    Make data look ok

--- src/main/java/ru/ntdv/proicis/graphql/controller/HelloController.java
+++ src/main/java/ru/ntdv/proicis/graphql/controller/HelloController.java
@@ -12,7 +12,7 @@ import static java.time.OffsetDateTime.now;
 @RestController
 public
 class HelloController {
-    @RequestMapping(path = "/getTime", produces = "application/json; charset=utf-8")
+    @RequestMapping(value = "/getTime", produces = "charset=utf-8")
     public
     String getCurrentUserDateTime(TimeZone timeZone) {
         OffsetDateTime data = now(timeZone.toZoneId());
