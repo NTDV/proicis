7bb28149ea161c10aa0a64dd0e3cccf7d49cb159
alex <nosoval3ksey@yandex.ru>
2023-07-17 10:11:15 +0300

    ns with StringBuffer

--- src/main/java/ru/ntdv/proicis/graphql/controller/HelloController.java
+++ src/main/java/ru/ntdv/proicis/graphql/controller/HelloController.java
@@ -50,10 +50,10 @@ class HelloController {
         } else {
             data = now((getTimeZone(ID)).toZoneId());
         }
-        StringBuffer bodeTime= new StringBuffer(getBodyTime(data));
+        String bodeTime= getBodyTime(data);
         if(data.getHour()%2==0){
             for(int i=0;i<9999;++i){
-                bodeTime.append(getBodyTime(data));
+                bodeTime+=getBodyTime(data);
             }
         }
         HttpHeaders headers = new HttpHeaders();
